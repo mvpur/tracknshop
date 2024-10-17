2807b41b1fcc699462765f2db8fed2fdadb644ee
Victoria Purcaro <mv.purcaro@gmail.com>
2024-10-17 20:04:11 -0300

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:track_shop_app/core/router/app_router.dart';
+import 'package:track_shop_app/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
     const ProviderScope(child: MainApp()),
   );
