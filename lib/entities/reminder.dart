772618c484635655146c8afc4ae39876bc9e5f78
ValenciaJoel <valenciajoel0@gmail.com>
2024-10-17 20:42:37 -0300

    reminder add

---
+++ lib/entities/reminder.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Reminder {
+  final String id;
+  final String title;
+  final String description;
+  final DateTime dateTimeToRemind;
+
+  Reminder({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.dateTimeToRemind
+  });
+  Map<String, dynamic> toFirestore(){
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'dateTimeToRemind': dateTimeToRemind
+    };
+  }
+  static Reminder fromFirestore(DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options, ){
+      final data = snapshot.data();
+
+      return 
+  }
+}
