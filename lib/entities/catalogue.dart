df71c89aa3a14e00790e1f831396bd8ab11eb0a6
ValenciaJoel <valenciajoel0@gmail.com>
2024-10-17 21:34:40 -0300

    catalogue update firestore

--- lib/entities/catalogue.dart
+++ lib/entities/catalogue.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class Catalogue {
@@ -11,4 +12,22 @@ class Catalogue {
       required this.icon,
       required this.name,
       required this.date});
+
+  Map<String, dynamic> toFirestore(){
+    return {
+      'id': id,
+      'name': name,
+      'icon': icon,
+      'date': date
+    };
+  }
+  static Catalogue fromFirestore(DocumentSnapshot<Map<String, dynamic>> snapshot,
+    SnapshotOptions? options, ){
+      final data = snapshot.data();
+
+      return Catalogue(id: data?['id'], 
+      name: data?['name'],
+      icon: data?['icon'], 
+      date: data?['date']);
+  }
 }
